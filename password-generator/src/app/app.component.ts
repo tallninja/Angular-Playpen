d6d38cd576df79cf3d1ec45d3f6e60779076b772
Ernest Wambua <ernestwambua2@gmail.com>
2022-12-17 19:53:09 +0300

    Property binding syntax

--- password-generator/src/app/app.component.ts
+++ password-generator/src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
   title = 'password-generator';
+  password: string = '';
+
+  generatePassword() {
+    this.password = 'PASSWORD';
+  }
 }
