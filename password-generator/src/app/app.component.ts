9d64113fc19934af1bb050e64da4beac19e9688b
Ernest Wambua <ernestwambua2@gmail.com>
2022-12-17 20:20:55 +0300

    Tracking input state

--- password-generator/src/app/app.component.ts
+++ password-generator/src/app/app.component.ts
@@ -7,9 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'password-generator';
+  length: number = 8;
+  letters: boolean = true;
+  numbers: boolean = true;
+  symbols: boolean = false;
   password: string = '';
+
+  setLength(e: Event) {}
+
+  setLetters() {
+    this.letters = !this.letters;
+  }
+
+  setNumbers() {
+    this.numbers = !this.numbers;
+  }
+
+  setSymbols() {
+    this.symbols = !this.symbols;
+  }
 
   generatePassword() {
     this.password = 'PASSWORD';
+    console.info(this.letters);
+    console.info(this.numbers);
+    console.info(this.symbols);
   }
 }
