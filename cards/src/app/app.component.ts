ae2a2f32576e9273bb285b1527d3fc5a977790c4
Ernest Wambua <ernestwambua2@gmail.com>
2022-12-21 20:20:52 +0300

    Accessing child components from parent components using property binding

--- cards/src/app/app.component.ts
+++ cards/src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    { title: 'Lorem Ipsum', image: 'assets/image1.jpg' },
+    { title: 'Hello World', image: 'assets/image2.jpg' },
+    { title: 'Love Life', image: 'assets/image3.jpg' },
+  ];
 }
