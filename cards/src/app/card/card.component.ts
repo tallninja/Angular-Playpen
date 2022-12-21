44a7cf2c4baab8ac69339978d94fb69c633a4cf7
Ernest Wambua <ernestwambua2@gmail.com>
2022-12-21 20:20:52 +0300

    Accessing child components from parent components using property binding

---
+++ cards/src/app/card/card.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.sass'],
+})
+export class CardComponent {
+  @Input() title: string = '';
+  @Input() image: string = '';
+}
